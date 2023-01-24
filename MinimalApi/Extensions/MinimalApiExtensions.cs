c2f1a8b80653460dd90887e96c12ef8a7764980f
Clive Harding <clive.harding@mercedes-benz.com>
2023-01-24 07:47:09 +0200

    Add project files.

---
+++ MinimalApi/Extensions/MinimalApiExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Application.Abstractions;
+using Application.Posts.Commands;
+using DataAccess.Repositories;
+using DataAccess;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Abstractions;
+
+namespace MinimalApi.Extensions
+{
+    public static class MinimalApiExtensions
+    {
+        public static void RegisterServices(this WebApplicationBuilder builder)
+        {
+            var connectionString = builder.Configuration.GetConnectionString("Default");
+            builder.Services.AddDbContext<SocialDbContext>(opt => opt.UseSqlServer(connectionString));
+            builder.Services.AddScoped<IPostRepository, PostRepository>();
+            builder.Services.AddMediatR(typeof(CreatePost));
+        }
+
+        public static void RegisterEndpointDefinitions(this WebApplication app)
+        {
+            var endpointDefinitions = typeof(Program).Assembly
+                .GetTypes()
+                .Where(t => t.IsAssignableTo(typeof(IEndpointDefinition))
+                    && !t.IsAbstract && !t.IsInterface)
+                .Select(Activator.CreateInstance)
+                .Cast<IEndpointDefinition>();
+
+            foreach (var endpointDefinition in endpointDefinitions)
+            {
+                endpointDefinition.RegisterEndpoints(app);
+            }
+        }
+    }
+}
